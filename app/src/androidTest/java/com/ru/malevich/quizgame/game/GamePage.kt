0ae9299af30e0e28b03c630a25f4d17842dfce4c
Danila <rexerofficial@gmail.com>
2024-08-30 19:15:10 +0500

    QG-12 page object game over screen

--- app/src/androidTest/java/com/ru/malevich/quizgame/game/GamePage.kt
+++ app/src/androidTest/java/com/ru/malevich/quizgame/game/GamePage.kt
@@ -2,6 +2,8 @@ package com.ru.malevich.quizgame.game
 
 import android.view.View
 import android.widget.LinearLayout
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
@@ -125,7 +127,7 @@ class GamePage(
         nextUi.click()
     }
 
-    fun assertNotVisible() {
-        TODO("Not yet implemented")
+    fun assertDoesNotExist() {
+        onView(containerIdMatcher).check(doesNotExist())
     }
 }
